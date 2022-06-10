c0a4a43c2e9374056abc4e0683ec966c6995c235
Simran Preet Singh <simran5590@gmail.com>
2022-06-10 07:58:32 -0700

    Setting up a ScrollView using AutoLayout in its content

--- ScrollViewPractice/ViewController.swift
+++ ScrollViewPractice/ViewController.swift
@@ -12,29 +12,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scrollView = UIScrollView(frame: self.view.bounds)
-        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.view.addSubview(scrollView)
-        scrollView.backgroundColor = .lightGray
+        // Initializing ScrollView
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .systemGray4
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        var y: CGFloat = 10
+        // Adding SV to ViewController's `view`
+        view.addSubview(scrollView)
         
-        for i in 0..<300 {
+        // Setting SV's constraints
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+        
+        // A label to keep the track of last label added to ScrollView
+        var previousLabel: UILabel? = nil
+        
+        // Creating 300 labels and addingi to Scroll View
+        for i in 1...300 {
             let label = UILabel()
-            label.text = "This is label \(i+1)"
-            label.sizeToFit()
-            label.frame.origin = CGPoint(x: 10, y: y)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "This is label \(i)"
             scrollView.addSubview(label)
-            y += label.bounds.size.height + 10
+            
+            label.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10).activate()
+            label.topAnchor.constraint(equalTo: previousLabel?.bottomAnchor ?? scrollView.topAnchor, constant: 10).activate()
+            
+            previousLabel = label
         }
         
-        var scrollViewSize = scrollView.bounds.size
-        print("Scroll View Height: ", scrollViewSize.height)
-        scrollViewSize.height = y
-        print("Scroll View Height Now: ", scrollViewSize.height)
-        scrollView.contentSize = scrollViewSize
+        // Attaching the last label to the bottom of scroll view to auto calculate the content size of the scroll view
+        if let previousLabel = previousLabel {
+            previousLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 10).activate()
+            previousLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).activate()
+        }
+        
     }
 
 
 }
 
+extension NSLayoutConstraint {
+    
+    func activate() {
+        isActive = true
+    }
+    
+    func deActivate() {
+        isActive = false
+    }
+}
