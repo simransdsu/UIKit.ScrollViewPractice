f407348fd4d1ebfbbb603bad53b006ed265cd2bf
Simran Preet Singh <simran5590@gmail.com>
2022-06-10 07:46:26 -0700

    Manual Content Size for Scroll View

--- ScrollViewPractice/ViewController.swift
+++ ScrollViewPractice/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(scrollView)
+        scrollView.backgroundColor = .lightGray
+        
+        var y: CGFloat = 10
+        
+        for i in 0..<300 {
+            let label = UILabel()
+            label.text = "This is label \(i+1)"
+            label.sizeToFit()
+            label.frame.origin = CGPoint(x: 10, y: y)
+            scrollView.addSubview(label)
+            y += label.bounds.size.height + 10
+        }
+        
+        var scrollViewSize = scrollView.bounds.size
+        print("Scroll View Height: ", scrollViewSize.height)
+        scrollViewSize.height = y
+        print("Scroll View Height Now: ", scrollViewSize.height)
+        scrollView.contentSize = scrollViewSize
     }
 
 
