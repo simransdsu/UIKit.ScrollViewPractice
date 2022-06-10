58fa427f36c1d1d82a476f283c8c81a90cdb86a0
Simran Preet Singh <simran5590@gmail.com>
2022-06-10 07:14:04 -0700

    Initial Commit

---
+++ ScrollViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewPractice
+//
+//  Created by Simran Preet Narang on 2022-06-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
